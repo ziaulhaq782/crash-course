820b55b29a032252945ecb9f4885ae7f45154124
ziaulhaq Nayab <nayabzia07@gmail.com>
2024-10-19 22:34:10 +0530

    Added isAbovea 8

--- index.js
+++ index.js
@@ -3,5 +3,9 @@ const name = 'ziaulhaq Nayab'
 const  dob = '01/06/1991'  
 
 function sayname() {
-    console.log('My Name is' + )
+    console.log('My Name is' + name1 )
+}
+
+function isAbove18 () {
+    return age >= 18
 }
