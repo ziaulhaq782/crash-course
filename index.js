5a6ea01bf6fada2388bef6bfec9ecd59b8f2efa5
ziaulhaq Nayab <nayabzia07@gmail.com>
2024-10-19 22:56:31 +0530

    Revert something

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const name = 'ziaulhaq Nayab'
 const  dob = '01/06/1991'  
 
 function sayname() {
+<<<<<<< HEAD
     console.log('My Name is' + name1 )
 }
 
@@ -12,4 +13,7 @@ function isAbove18 () {
 
 function newFn() {
     console.log('Hey I am a new fn')
+=======
+    console.log('My Name is' + )
+>>>>>>> parent of 820b55b (Added isAbovea 8)
 }
